40afa22449aec1ae6812047004414ae12847f0aa
dendinge21 <dendinge21@up.edu>
2019-01-14 21:28:01 -0800

    Buttons

--- app/src/main/java/edu/up/cs371/epp/photofunpattern/PhotoFun.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/PhotoFun.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
         import android.widget.ImageView;
         import android.widget.Button;
         import android.view.View;
+import android.widget.TextView;
 
 /**
  *  class PhotoFun controls this photo manipulation app.
@@ -59,6 +60,10 @@ public class PhotoFun extends AppCompatActivity {
         public void onClick(View button) {
             GrayFilter filter = new GrayFilter();
             myNewImageView.setImageBitmap(filter.apply(myOriginalBmp));
+
+            TextView modified = (TextView) findViewById(R.id.modified);
+            modified.setText("Gray Scaled");
+
         }
     }
 
@@ -72,6 +77,10 @@ public class PhotoFun extends AppCompatActivity {
         public void onClick(View button) {
             BrightnessFilter filter = new BrightnessFilter();
             myNewImageView.setImageBitmap(filter.apply(myOriginalBmp));
+
+            TextView modified = (TextView) findViewById(R.id.modified);
+            modified.setText("Brightened");
+
         }
     }
 }
